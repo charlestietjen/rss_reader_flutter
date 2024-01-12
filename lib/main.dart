1eddbde6f65960f79fac21f85d9acba8a9dbd9e5
Charles <tietjen.charles@gmail.com>
2024-01-12 13:16:52 -0500

    fetch feeds, display rows, clickable entries, page setup

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:webfeed/webfeed.dart';
 import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(const MainApp());
+}
+
+Future<void> openBrowser(url) async {
+  final uri = Uri.parse(url);
+  if (!await launchUrl(uri)) {
+    throw Exception('Could not launch $uri');
+  }
 }
 
 class MainApp extends StatelessWidget {
@@ -18,40 +26,162 @@ class MainApp extends StatelessWidget {
             title: 'Rss-Reader',
             theme: ThemeData(
               useMaterial3: true,
-              colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+              colorScheme: ColorScheme.fromSeed(
+                  seedColor: Colors.cyan, background: Colors.transparent),
             ),
-            home: FeedPage()));
+            home: App()));
   }
 }
 
 class AppState extends ChangeNotifier {
-  var url = 'https://godotengine.org/rss.xml';
+  var urls = [
+    'https://godotengine.org/rss.xml',
+    'https://news.ycombinator.com/rss'
+  ];
 
-  var rssEntries = [];
+  var rssEntries = <RssItem>[];
 
   void fetchRssEntries() async {
-    var res = await http.get(Uri.parse(url));
-    var rssResults = RssFeed.parse(res.body);
     rssEntries = [];
-    for (var entry in rssResults.items!) {
-      rssEntries.add(entry.title);
+    for (var feed in urls) {
+      var res = await http.get(Uri.parse(feed));
+      var rssResults = RssFeed.parse(res.body);
+      print('${res.statusCode}: ${rssResults}');
+      for (var entry in rssResults.items!) {
+        rssEntries.add(entry);
+      }
     }
+    rssEntries.sort((a, b) => -a.pubDate!.compareTo(b.pubDate!));
     notifyListeners();
   }
 }
 
-class FeedPage extends StatelessWidget {
+class App extends StatefulWidget {
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  var selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = FeedPage();
+        break;
+      default:
+        throw UnimplementedError('no widget for ${selectedIndex}');
+    }
+    return LayoutBuilder(builder: (context, constraints) {
+      return Scaffold(
+          body: Row(
+        children: [
+          SafeArea(
+            child: NavigationRail(
+              selectedIndex: selectedIndex,
+              destinations: [
+                NavigationRailDestination(
+                  label: const Text('Home'),
+                  icon: const Icon(Icons.rss_feed),
+                ),
+                NavigationRailDestination(
+                    icon: Icon(Icons.settings), label: Text('Settings')),
+              ],
+            ),
+          ),
+          Expanded(child: page)
+        ],
+      ));
+    });
+  }
+}
+
+class FeedPage extends StatelessWidget {
+  const FeedPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+
+    final style =
+        theme.textTheme.bodyLarge!.copyWith(color: theme.colorScheme.onPrimary);
     var appState = context.watch<AppState>();
     var rssEntries = appState.rssEntries;
+
     if (rssEntries.isNotEmpty) {
-      return ListView(
-        children: [for (var entry in rssEntries) Text(entry.toString())],
+      return Container(
+        color: Theme.of(context).colorScheme.primary,
+        child: Padding(
+          padding: const EdgeInsets.only(left: 40.0, top: 20.0),
+          child: ListView(
+            children: [
+              for (var entry in rssEntries)
+                MouseRegion(
+                  cursor: SystemMouseCursors.click,
+                  child: GestureDetector(
+                    key: entry.guid != null
+                        ? Key(entry.guid!)
+                        : Key(entry.link!),
+                    child: Row(
+                      children: [
+                        entry.source != null
+                            ? Flexible(
+                                flex: 1,
+                                child: SizedBox(
+                                  width: 300,
+                                  child: Text(entry.source.toString(),
+                                      style: style,
+                                      overflow: TextOverflow.clip,
+                                      textAlign: TextAlign.center),
+                                ),
+                              )
+                            : Flexible(
+                                flex: 1,
+                                child: SizedBox(
+                                  width: 300,
+                                  child: Text(entry.link.toString(),
+                                      style: style,
+                                      overflow: TextOverflow.clip,
+                                      textAlign: TextAlign.center),
+                                )),
+                        const SizedBox(width: 20),
+                        Flexible(
+                            flex: 1,
+                            child: SizedBox(
+                              width: 800,
+                              child: Text(
+                                entry.title.toString(),
+                                style: style,
+                                overflow: TextOverflow.clip,
+                              ),
+                            )),
+                        const SizedBox(width: 20),
+                        Flexible(
+                          flex: 1,
+                          child: SizedBox(
+                            width: 100,
+                            child: Text(
+                              '${entry.pubDate!.day}/${entry.pubDate!.month}/${entry.pubDate!.year}',
+                              style: style,
+                              textAlign: TextAlign.end,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    onTap: () => {openBrowser(entry.link)},
+                  ),
+                )
+            ],
+          ),
+        ),
       );
     } else {
       appState.fetchRssEntries();
-      return (Text('No Entries Available'));
+      return Container(
+          color: Theme.of(context).colorScheme.primary,
+          child: (Text('No Entries Available', style: style)));
     }
   }
 }
